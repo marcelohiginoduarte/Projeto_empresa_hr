64bae51dace32cefb9a3596162c736daea6922a4
marcelohiginoduarte <marcelihigino@gmail.com>
2024-09-19 11:50:21 -0300

    Refatorando o admin

--- GestaoHR/admin.py
+++ GestaoHR/admin.py
@@ -1,45 +1,60 @@
 from django.contrib import admin
 from GestaoHR.models import collaborator, Aquivo, Servico, BancoArquivos, DemandaInterna, SESMT, ArquivoSesmt, arquivos_foto, FotosCampo
 
-admin.site.register(collaborator)
+
 
 class ListandoCollaborator(admin.ModelAdmin):
     list_display = ("Nome", "CPF", "RG")
+    search_fields = ("Nome",)
     list_display_links = ("Nome", "CPF")
 
-admin.site.register(Aquivo)
+admin.site.register(collaborator, ListandoCollaborator)
+
+
 
 class ListandoArquivo(admin.ModelAdmin):
     list_display=('Nome', 'Arquivo_ponto')
     list_display_links = ('Nome', 'Arquivo_ponto')
 
-admin.site.register(Servico)
+admin.site.register(Aquivo, ListandoArquivo) 
+
+
 
 class ListandoServico(admin.ModelAdmin):
     list_display = ('PEP', 'Municipio')
     list_display_links = ('PEP', 'Municipio')
 
-admin.site.register(BancoArquivos)
+admin.site.register(Servico, ListandoServico)
+
+
 
 class Listandobdarquivos(admin.ModelAdmin):
     list_display = ('EI_OC', 'municipio')
     list_display_links = ('EI_OC', 'municipio')
 
-    admin.site.register(DemandaInterna)
+admin.site.register(BancoArquivos, Listandobdarquivos)
+
+
 
 class ListandoDemandaInterna(admin.ModelAdmin):
     list_display = ('Atividade', 'tipo')
     list_display_links = ('Atividade', 'tipo')
 
-admin.site.register(SESMT)
-class ListandoSESMT(admin.ModelAdmin):
-    list_display = ('Atividade', 'tipo')
-    list_display_links = ('Atividade', 'tipo')
+admin.site.register(DemandaInterna, ListandoDemandaInterna)
 
-admin.site.register(ArquivoSesmt)
+
+class ListandoSESMT(admin.ModelAdmin):
+    list_display = ('Ultima_atualização', 'Ultima_atualização')
+    list_display_links = ('Ultima_atualização', 'Ultima_atualização')
+
+admin.site.register(SESMT, ListandoSESMT)
+
+
 class ArquivosLista(admin.ModelAdmin):
-    list_display = ('Atividade', 'tipo')
-    list_display_links = ('Atividade', 'tipo')
+    list_display = ('Nome', 'data_envio')
+    list_display_links = ('Nome', 'data_envio')
+
+admin.site.register(ArquivoSesmt, ArquivosLista)
 
 admin.site.register(arquivos_foto)
 admin.site.register(FotosCampo)
