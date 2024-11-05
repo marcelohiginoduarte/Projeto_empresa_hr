bff4cc44effe7df8dada60167490e4d4aca3318f
marcelohiginoduarte <marcelihigino@gmail.com>
2024-11-05 08:54:12 -0300

    Ajustando o laytou da programação equipes

--- Setup/urls.py
+++ Setup/urls.py
@@ -86,10 +86,10 @@ urlpatterns = [
 
     path('programacao/cadstro', views.cadastrar_programacaoequipe, name='cadastrarprogramacapequipe'),
     path('programacao/vertoda', views.ver_programacao, name='vertodaprogramacao'),
-    path('programacao/calendario', views.calendario_view, name='vertodaprogramacao'),
+    path('#', views.calendario_view, name='vertodaprogramacaoemcalendario'),
     path('programacao/atualizar/<int:pk>', views.AtualizarProgramacaoEquipes.as_view(), name='atualizarprogramacão'),
     path('programacao/deletar/<int:pk>', views.DeletarProgramacaoEquipes.as_view(), name='deletarprogramacao'),
-
+    path('calendario-dados/', views.calendario_dados, name='calendario_dados'),
 
     path('enviodefotoapi', views.FotoUploadView.as_view(), name='upload-fotodecanoi'),
     
